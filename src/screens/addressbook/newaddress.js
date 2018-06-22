80a667d9c545958457472bac9353c32176db1210
Greg Marlin <greg@transformation.ai>
2018-06-22 19:32:10 -0400

    updated NewAddress

--- src/screens/addressbook/newaddress.js
+++ src/screens/addressbook/newaddress.js
@@ -13,12 +13,101 @@ import {
   Right,
   Icon,
   Form,
-  Text
+  Picker,
+  Spinner,
+  InputGroup,
+  List,
+  ListItem
 } from "native-base";
+import {
+  TouchableHighlight,
+  Text,
+  ToastAndroid
+} from 'react-native';
 import styles from "./styles";
+import addressService from './addressService';
+import addressDetail from './addressDetail';
 
 class NewAddress extends Component {
+  constructor(props) {
+    super(props);
+
+    let editObj = this.props.route.editObj;
+    let editIsNotNull = editObj != null;
+    this.state = {
+      //used to display a progress indicator if waiting for a network response
+      loading: false,
+      id: editIsNotNull ? editObj.id : null,
+      description: editIsNotNull ? editObj.description : ''
+      }
+  }
+
+  save() {
+    this.setState({
+      loading: true
+    });
+   var hasError = false;
+    if(this.state.label === ''){
+      ToastAndroid.show('Label is required', ToastAndroid.LONG);
+      hasError = true;
+    }
+    if(this.state.address === ''){
+      ToastAndroid.show('Address is required', ToastAndroid.LONG);
+      hasError = true;
+    }
+ 
+    if(hasError){
+      this.setState({
+        loading: false
+      });
+      return;
+    }
+
+  var cond = {
+    address: this.state.address,
+    label: this.state.label
+  };
+  var saved = addressService.save(cond);
+
+  ToastAndroid.show('Saved!', ToastAndroid.LONG);
+
+  this.props.route.callbackUpdateList();
+    if(this.state.address == null){
+      this.props.navigator.replace({
+        component: addresskDetail,
+        callbackUpdateList: this.props.route.callbackUpdateList,
+        obj: saved
+      });
+    }else{
+      this.props.navigator.pop();
+    }
+ 
+  }
+  back = () => {
+    this.props.navigator.pop();
+  }
+
+
   render() {
+    const content = this.state.loading ? <content> <spinner color="blue"> </spinner></content> :
+    <content keyboarddismissmode="interactive" keyboardshouldpersisttaps="{true}">
+          <list>
+             <listitem>
+                 <inputgroup>
+                               <input onchangetext={(text) ==> this.setState({label: text})}
+                                 value={this.state.label}
+                                 stackedLabel label="Label"
+                               autoFocus={true}/>
+                           </inputgroup>
+                       </listitem>
+ 
+ 
+ 
+                   </list><button block success onPress={this.save()} style={{ alignSelf: 'center', marginTop: 20, marginBottom: 20 }}>
+                       Save
+                   </button>
+                 </content>;
+
     return (
       <Container style={styles.container}>
         <Header>
@@ -37,22 +126,7 @@ class NewAddress extends Component {
           </Right>
         </Header>
 
-        <Content>
-          <Form>
-            <Text>Name</Text>
-            <Item>
-              <Input placeholder="Add name" />
-            </Item>
-            <Text>Address</Text>
-           <Item>
-              <Input placeholder="Write address or label name" />
-            </Item>
-           
-          </Form>
-         
-
-          
-        </Content>
+       {content}
       </Container>
     );
   }
