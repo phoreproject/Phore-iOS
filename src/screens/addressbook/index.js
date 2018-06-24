3edffba2fdaff5745e14977f3ba7a7edfc658f24
user142513 <greg@growth-hackers.com>
2018-06-23 18:12:40 -0700

    updated address book

--- src/screens/addressbook/index.js
+++ src/screens/addressbook/index.js
@@ -22,34 +22,11 @@ import {
 import styles from "./styles";
 
 import NewAddress from './newaddress';
-import addressDetail from './addressDetail';
-import addressService from './addressService';
-import addressItem from './addressItem';
+
 
 class AddressBook extends Component {
-  constructor(props) {
-    super(props);
-
-    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-        this.state = {
-          dataSource: ds.cloneWithRows([
-            {description: 'initial'}
-            ]),
-          search: "",
-          refreshing: false,
-        };
-  }
-
-  showItem(obj){
-    this.props.navigator.push({
-      component: addressDetail,
-      callbackUpdateList: this.callbackUpdateList,
-      obj: obj
-    });
-  }
-  componentWillMount(){
-    this.updateList();
-  }
+  
+    
   render() {
     return (
       <Container style={styles.container}>
@@ -78,13 +55,7 @@ class AddressBook extends Component {
       
          <Content>
 
-                 <ListView datasource="{this.state.dataSource}" 
-                 enableemptysections="{true}" 
-                 renderrow="{this._renderItem.bind(this)}" 
-                 style="{styles.listView}" 
-                 refreshcontrol={ <refreshcontrol refreshing="{this.state.refreshing}" onrefresh="{this.updateList.bind(this)}"
-                 />}>
-               </ListView>
+                
                
 
           </Content>
@@ -97,43 +68,7 @@ class AddressBook extends Component {
     );
   }
 
-  updateList(){
-    this.setState({refreshing: true});
-    var all = addressService.search(this.state.search);
-    this.setState({
-      dataSource: this.state.dataSource.cloneWithRows(all)
-    });
-    this.setState({refreshing: false});
-  }
-
-  _onActionSelected = (position) => {
-    console.log(position);
-    if(position == 0){
-      this.props.navigator.push({
-        component: NewAddess,
-        callbackUpdateList: this.callbackUpdateList,
-      });
-    }
-  };
-
-  callbackUpdateList = () => {
-    console.log('callbackUpdateList');
-    this.updateList();
-  }
-
-  _renderItem(obj) {
-    if(obj == null){
-      return null;
-    }
-    return (
-     <touchablehighlight onPress={this.showItem(obj)}>
-        <view>
-          <addressitem obj={obj}>
-        </addressitem>
-        </view>
-     </touchablehighlight>
-    );
-  }
+  
 
 
 }
--- src/screens/addressbook/newaddress.js
+++ src/screens/addressbook/newaddress.js
@@ -25,88 +25,13 @@ import {
   ToastAndroid
 } from 'react-native';
 import styles from "./styles";
-import addressService from './addressService';
-import addressDetail from './addressDetail';
+
 
 class NewAddress extends Component {
-  constructor(props) {
-    super(props);
-
-    let editObj = this.props.route.editObj;
-    let editIsNotNull = editObj != null;
-    this.state = {
-      //used to display a progress indicator if waiting for a network response
-      loading: false,
-      id: editIsNotNull ? editObj.id : null,
-      description: editIsNotNull ? editObj.description : ''
-      }
-  }
-
-  save() {
-    this.setState({
-      loading: true
-    });
-   var hasError = false;
-    if(this.state.label === ''){
-      ToastAndroid.show('Label is required', ToastAndroid.LONG);
-      hasError = true;
-    }
-    if(this.state.address === ''){
-      ToastAndroid.show('Address is required', ToastAndroid.LONG);
-      hasError = true;
-    }
- 
-    if(hasError){
-      this.setState({
-        loading: false
-      });
-      return;
-    }
-
-  var cond = {
-    address: this.state.address,
-    label: this.state.label
-  };
-  var saved = addressService.save(cond);
-
-  ToastAndroid.show('Saved!', ToastAndroid.LONG);
-
-  this.props.route.callbackUpdateList();
-    if(this.state.address == null){
-      this.props.navigator.replace({
-        component: addresskDetail,
-        callbackUpdateList: this.props.route.callbackUpdateList,
-        obj: saved
-      });
-    }else{
-      this.props.navigator.pop();
-    }
- 
-  }
-  back = () => {
-    this.props.navigator.pop();
-  }
-
+  
 
   render() {
-    const content = this.state.loading ? <content> <spinner color="blue"> </spinner></content> :
-    <content keyboarddismissmode="interactive" keyboardshouldpersisttaps="{true}">
-          <list>
-             <listitem>
-                 <inputgroup>
-                               <input onchangetext={(text) ==> this.setState({label: text})}
-                                 value={this.state.label}
-                                 stackedLabel label="Label"
-                               autoFocus={true}/>
-                           </inputgroup>
-                       </listitem>
- 
- 
- 
-                   </list><button block success onPress={this.save()} style={{ alignSelf: 'center', marginTop: 20, marginBottom: 20 }}>
-                       Save
-                   </button>
-                 </content>;
+    
 
     return (
       <Container style={styles.container}>
