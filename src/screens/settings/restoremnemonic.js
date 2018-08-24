073dbc2215147ddb1d8159cc59c2daa37fa8ad25
user142513 <greg@growth-hackers.com>
2018-08-24 15:36:05 -0700

    update wallet import from mnemonic

--- src/screens/settings/restoremnemonic.js
+++ src/screens/settings/restoremnemonic.js
@@ -13,22 +13,67 @@ import {
   Right,
   Icon,
   Form,
+  Textarea,
   Text
 } from "native-base";
+import { StatusBar } from 'react-native';
 import styles from "./styles";
+import * as bip39 from '../../components/bip39';
+import * as phore from '../../wallet';
+import * as RealmDB from '../../realm/RealmSchemas';
+import * as hdkey from '../../components/hdkey';
+import * as uuid from "react-native-uuid";
 
 class RestoreMnemonic extends Component {
+
+  state = {
+      inputcode: '',
+      seed: '',
+      hdmaster: '',
+      keypair: '',
+      WIF: '',
+      pubkey: '',
+      address: '',
+      xpub: '',
+      xprv: ''
+      
+    }
+
+
+  importWallet = () => {
+      RealmDB.realm.write(() => {
+        var wid = uuid.v1()
+        var seed = bip39.generateSeed(this.state.inputcode)
+        var hdmaster = phore.generateHDMaster(seed)
+        var keypair = phore.generateKeyPairFromMaster(hdmaster, 1)
+        var WIF = phore.getWIFfromKeyPair(keypair)
+        var recaddress = phore.getAddressFromKeyPair(keypair)
+        var hdk = hdkey.rnHDKey(seed)
+        var xpub = hdk.publicExtendedKey
+        var xprv = hdk.privateExtendedKey
+        RealmDB.realm.create('FullWallet', {
+          wid: wid,
+          seed: seed,
+          WIF: WIF,
+          recaddress: recaddress,
+          xpub: xpub,
+          xprv: xprv
+        })
+      })
+    }
+
   render() {
     return (
       <Container style={styles.container}>
         <Header>
+        <StatusBar barStyle="light-content" />
           <Left>
             <Button transparent onPress={() => this.props.navigation.goBack()}>
-              <Icon name="arrow-back" />
+              <Icon name="arrow-back" style={{ color: 'white'}}/>
             </Button>
           </Left>
           <Body>
-            <Title>Restore Mnemonic</Title>
+            <Title style={{ color: 'white'}}>Restore Mnemonic</Title>
           </Body>
           <Right>
             
@@ -39,84 +84,25 @@ class RestoreMnemonic extends Component {
           
           <Text style={{alignSelf: 'center'}}>To restore your wallet you must enter the 24 words of your mnemonic code in the right order</Text>
           <Form>
-          <Item regular style={{ marginLeft: 15, margin: 15, marginTop: 15 }}>
-              <Input placeholder="" />
-          </Item>
-          
-          <Item regular style={{ marginLeft: 15, margin: 15, marginTop: 15 }}>
-              <Input placeholder="" />
-          </Item>
-          <Item regular style={{ marginLeft: 15, margin: 15, marginTop: 15 }}>
-              <Input placeholder="" />
-          </Item>
-          <Item regular style={{ marginLeft: 15, margin: 15, marginTop: 15 }}>
-              <Input placeholder="" />
-          </Item>
-          <Item regular style={{ marginLeft: 15, margin: 15, marginTop: 15 }}>
-              <Input placeholder="" />
-          </Item>
-          <Item regular style={{ marginLeft: 15, margin: 15, marginTop: 15 }}>
-              <Input placeholder="" />
-          </Item>
-          <Item regular style={{ marginLeft: 15, margin: 15, marginTop: 15 }}>
-              <Input placeholder="" />
-          </Item>
-         <Item regular style={{ marginLeft: 15, margin: 15, marginTop: 15 }}>
-              <Input placeholder="" />
-          </Item>
-          <Item regular style={{ marginLeft: 15, margin: 15, marginTop: 15 }}>
-              <Input placeholder="" />
-          </Item>
-          
-          <Item regular style={{ marginLeft: 15, margin: 15, marginTop: 15 }}>
-              <Input placeholder="" />
-          </Item>
-          <Item regular style={{ marginLeft: 15, margin: 15, marginTop: 15 }}>
-              <Input placeholder="" />
-          </Item>
-          <Item regular style={{ marginLeft: 15, margin: 15, marginTop: 15 }}>
-              <Input placeholder="" />
-          </Item>
-          <Item regular style={{ marginLeft: 15, margin: 15, marginTop: 15 }}>
-              <Input placeholder="" />
-          </Item>
-          <Item regular style={{ marginLeft: 15, margin: 15, marginTop: 15 }}>
-              <Input placeholder="" />
-          </Item>
-          <Item regular style={{ marginLeft: 15, margin: 15, marginTop: 15 }}>
-              <Input placeholder="" />
-          </Item>
-         <Item regular style={{ marginLeft: 15, margin: 15, marginTop: 15 }}>
-              <Input placeholder="" />
-          </Item>
-          <Item regular style={{ marginLeft: 15, margin: 15, marginTop: 15 }}>
-              <Input placeholder="" />
-          </Item>
-          
-          <Item regular style={{ marginLeft: 15, margin: 15, marginTop: 15 }}>
-              <Input placeholder="" />
-          </Item>
-          <Item regular style={{ marginLeft: 15, margin: 15, marginTop: 15 }}>
-              <Input placeholder="" />
-          </Item>
-          <Item regular style={{ marginLeft: 15, margin: 15, marginTop: 15 }}>
-              <Input placeholder="" />
-          </Item>
-          <Item regular style={{ marginLeft: 15, margin: 15, marginTop: 15 }}>
-              <Input placeholder="" />
-          </Item>
-          <Item regular style={{ marginLeft: 15, margin: 15, marginTop: 15 }}>
-              <Input placeholder="" />
-          </Item>
-          <Item regular style={{ marginLeft: 15, margin: 15, marginTop: 15 }}>
-              <Input placeholder="" />
-          </Item>
-         <Item regular style={{ marginLeft: 15, margin: 15, marginTop: 15 }}>
-              <Input placeholder="" />
-          </Item>
+        <Textarea rowSpan={10} bordered placeholder="Input words with space in between each"
+
+        Input 
+              onChangeText={ (inputcode) => this.setState({inputcode})}
+              value={this.state.inputcode} />
+
+        
+
 
           <Button bordered dark block style={{ margin: 15, marginTop: 15 }}
-           onPress={() => this.props.navigation.navigate("RestoreWallet")}>
+           onPress={() => {
+
+            console.log('inputcode is: ' + this.state.inputcode)
+            
+            this.importWallet()
+
+          }
+            
+          }>
             <Text>Confirm</Text>
           </Button>
 
